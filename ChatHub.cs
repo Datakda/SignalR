9d0c07d073e6607265d138767c4c0a3d33fbf26d
Datakda <gagan9393@gmail.com>
2021-05-31 13:01:09 +0300

    Добавьте файлы проекта.

---
+++ ChatHub.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using SignalR.Models;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace SignalR
+{
+    public class ChatHub : Hub
+    {
+
+        public async Task Send(string message)
+        {
+
+            MinMax rnd = new MinMax();
+
+            WebRequest GETrequest = WebRequest.Create("http://185.195.26.249:7777/GetPost");
+            WebResponse GETresponse = GETrequest.GetResponse();
+            using (Stream stream = GETresponse.GetResponseStream())
+            {
+                using (StreamReader reader = new StreamReader(stream))
+                {
+                    string line = "";
+                    line = reader.ReadLine();
+
+                    rnd = JsonSerializer.Deserialize<MinMax>(line); ;
+
+                }
+            }
+            GETresponse.Close();
+            await this.Clients.All.SendAsync("Send", $"Min = {rnd.Min}, Max = {rnd.Max}");
+            
+        }
+
+      
+    }
+}
---
+++ Models/MinMax.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SignalR.Models
+{
+    public class MinMax
+    {
+        public double Min { get; set; }
+        public double Max { get; set; }
+
+    }
+}
